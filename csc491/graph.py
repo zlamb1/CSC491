3ab4f9eaff94bdbde232b5385fc986b003667e9c
Zachary Scott Lamb <zlamb@carrollu.edu>
2026-02-19 12:07:23 -0600

    Created bar_count in graph.py.

--- csc491/graph.py
+++ csc491/graph.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 def log_returns(dfs, cols):
   fig, axs = plt.subplots(1, len(dfs), figsize=(15, 5))
@@ -14,4 +15,20 @@ def log_returns(dfs, cols):
     df.dropna(inplace=True)
     axs[i].hist(df['log_returns'], bins=100)
   
+  plt.show()
+
+def bar_count(dfs):
+  fig, axs = plt.subplots(len(dfs), figsize=(15, 5))
+  fig.suptitle('Bar Counts')
+
+  if len(dfs) == 1:
+    axs = [axs]
+
+  for i in range(len(dfs)):
+    df = dfs[i]
+    df = df.groupby(pd.Grouper(level='timestamp', freq='W')).size().rename('count').reset_index()
+    axs[i].bar(df['timestamp'], df['count'], width=5)
+    axs[i].tick_params(axis='x', rotation=45)
+
+  plt.tight_layout()
   plt.show()
